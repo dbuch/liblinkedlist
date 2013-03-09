7701bd7ca3dbb6902aedbdbab01cb2f0f19541ec
Daniel Buch <boogiewasthere@gmail.com>
2013-03-09 19:00:45 +0100

    GPL added to each src

--- src/linkedlist.c
+++ src/linkedlist.c
@@ -1,3 +1,22 @@
+/***
+  This file (linkedlist.c) is part of Liblinkedlist
+
+  Copyright 2013 Daniel Buch
+
+  Liblinkedlist is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  Liblinkedlist is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with Liblinkedlist; If not, see <http://www.gnu.org/licenses/>.
+***/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
