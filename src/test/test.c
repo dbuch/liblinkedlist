b96e34f4c6db595dc82783e07757127940f7ab92
Daniel Buch <boogiewasthere@gmail.com>
2013-03-12 13:58:22 +0100

    added test.c

---
+++ src/test/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "../linkedlist.h"
+
+static void test_list_sort(void) {
+        printf("test\n");
+}
+
+int main(int argc, const char *argv[])
+{
+        test_list_sort();
+        return 0;
+}
